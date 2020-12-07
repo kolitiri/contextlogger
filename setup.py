2bb4a75b61af3c072b7c6f24593187c65ae51732
chris220688 <13407893+kolitiri@users.noreply.github.com>
2020-12-07 19:40:04 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setup(
 	name='contextlogger',
-	version='0.0.5',
+	version='0.0.6',
 	description='A logging boilerplate enhanced by the use of contextvars',
 	py_modules=['contextlogger', 'exceptions'],
 	package_dir={'': 'src'},
