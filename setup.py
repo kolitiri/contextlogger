20f98b2762c788f4cbc8f49e3960533f652f4f27
chris220688 <13407893+kolitiri@users.noreply.github.com>
2021-02-21 15:29:24 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,17 +6,23 @@ with open('README.md', 'r') as fh:
 
 setup(
 	name='contextlogger',
-	version='1.0.0',
+	version='1.0.1',
 	description='A logging boilerplate enhanced by the use of contextvars',
-	py_modules=['contextlogger', 'exceptions'],
-	package_dir={'': 'src'},
+	url='https://github.com/kolitiri/contextlogger',
+	author='Christos Liontos',
+	license='MIT',
 	classifiers=[
-		"Programming Language :: Python :: 3",
-		"Programming Language :: Python :: 3.6",
 		"Programming Language :: Python :: 3.7",
 		"License :: OSI Approved :: MIT License",
 		"Operating System :: OS Independent",
 	],
+	keywords='logging context',
+	project_urls={
+		'Source': 'https://github.com/kolitiri/contextlogger',
+		'Tracker': 'https://github.com/kolitiri/contextlogger/issues',
+	},
+	py_modules=['contextlogger', 'exceptions'],
+	package_dir={'': 'src'},
 	long_description=long_description,
 	long_description_content_type='text/markdown',
 	extras_require={
